6d50ba491088cc4fcc3d8ad9fd53c3fbaf45e00f
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-28 17:55:53 +0900

    강의내용(앱 분석)

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -8,14 +8,24 @@
 import Foundation
 
 var computerChoice = Int.random(in: 1...100)
+
 var myChoice:Int = 0
+    // 반복의 조건이 정해져있을때 while / 반복의 횟수가 있다면 for
     while true {
+        // readLine() 옵셔널 String 을 리턴함 
         let userInput = readLine()
         if let input = userInput {
             if let numInput = Int(input) {
                 myChoice = numInput
             }
         }
+        // if let input = userInput , let numInput = Int(input) 으로 조건 한줄로 써도 무관
+        // 만약 guard let 바인딩으로 쓰고싶다면
+        // guard let input = userInput, let numInput = Int(intput) else {
+        // print("break")
+        // break
+        // }
+        // myChoice = numInput
         
         if computerChoice > myChoice {
             print("UP")
